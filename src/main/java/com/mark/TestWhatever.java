826d5a665a346c3ae1aa4fadd1cc97167d45ccc8
markMe1024 <com.mark.yu2015@gmail.com>
2019-11-12 19:40:03 +0800

    修改

---
+++ src/main/java/com/mark/TestWhatever.java
@@ -0,0 +1,14 @@
+package com.mark;
+
+import java.util.UUID;
+
+/**
+ * @author mark
+ * @date 2019/11/12 19:36
+ */
+public class TestWhatever {
+    public static void main(String[] args) {
+        String uuid = UUID.randomUUID().toString();
+        System.out.println(uuid);
+    }
+}
